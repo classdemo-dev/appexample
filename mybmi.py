1497e692b546d29e1f31c0ed3b26593d84960216
classdemo-dev <codedemo202510@gmail.com>
2025-10-27 19:28:41 +0900

    Update mybmi.py

--- mybmi.py
+++ mybmi.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("BMI")
 #st.write("")
-st.text_input("input your name")
+name=st.text_input("input your name")
 h=st.number_input("Height")
 w=st.number_input("Weight")
 
@@ -10,3 +10,4 @@ if h > 0:
     st.write(f"{name}님의 BMI는 **{bmi:.2f}** 입니다.")
 else:
     st.warning("키를 0보다 큰 값으로 입력해주세요.")
+
